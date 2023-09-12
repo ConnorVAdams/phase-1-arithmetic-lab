9cc6286b490cef6c527c086ca6df26ceef5f1bc3
Connor Adams <connorvadams@gmail.com>
2023-09-11 20:13:21 -0600

    Creates various mathematical equations.

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+const num1 = 31;
+const num2 = 2;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 34
+const num4 = 10
+
+const mod = num3 % num4; 
+console.log(mod);
+
+const max = Math.max(12, 4, 5, 7, 11, 20);
